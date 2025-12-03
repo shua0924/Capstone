0e4bbae63e9c480560ef839186e395d23afb19f4
shua0924 <joshuacaballes12ict@gmail.com>
2025-12-03 13:07:19 +0800

    adding new database

--- wifi_hotspot.sql
+++ wifi_hotspot.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 02, 2025 at 03:24 PM
+-- Generation Time: Dec 03, 2025 at 06:05 AM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
